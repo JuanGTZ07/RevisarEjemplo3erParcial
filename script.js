5359a5f48f6aa92d18f21266b53b1ca5fe69fec0
JuanGTZ07 <al45677@un.edu.mx>
2025-11-11 09:30:18 -0600

    .

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ function calcularTotal(){
     var total = 0;
     var esp = document.getElementById("especialidad");
     var pan = formulario.pan.value;
-
+    var adi = formulario.ingrediente.checked;
+    
     if (pan == 0) {
         total += 0;
-        alert(total);
     }
     else if (pan == 20) {
         total += 20;
-        alert(total);
     }
+    else if (pan == 50) {
+        total += 50;
+    }
+    else {
+        alert("Seleccione el tipo de pan");
+    }
+
+    
+
+
+    alert(total);
 }
