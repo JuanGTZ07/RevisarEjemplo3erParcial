d1800865c69315342f1dc9d6ee53c70b4bf3967b
Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2025-11-11 16:03:07 -0600

    Se te indican los cambios para que el código estuviera al 100

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ var formulario = document.getElementById("pedidoForm");
 
 function calcularTotal(){
     var total = 0;
-    var esp = document.getElementById("especialidad");
+    var esp = formulario.especialidad.value;
     var pan = formulario.pan.value;
-    var ingredientes = formulario.ingredientes.cheked;
+    var esDomicilio = formulario.domicilio.checked;
 
-    if (esp == document.getElementById("clasica")) {
+    
+    if (esp == "clasica") {
         total += 80;
     }
-    else if ( esp == document.getElementById("bbq")) {
+    else if ( esp == "bbq") {
         total += 100;
     }
-    else if ( esp == document.getElementById("tocino")) {
+    else if ( esp == "tocino") {
         total += 120;
     }
-    else if ( esp == document.getElementById("doble")) {
+    else if ( esp == "doble") {
         total += 150;
     }
-    else if ( esp == document.getElementById("hawaiana")) {
+    else if ( esp == "hawaiana") {
         total += 180;
     }
     else {
-        alert("seleccione el tipo de pan")
+        alert("seleccione el tipo de pan");
+        return;
     }
 
     if (pan == 0) {
@@ -36,18 +38,38 @@ function calcularTotal(){
     }
     else {
         alert("Seleccione el tipo de pan");
+        return;
     }
 
-    var ingredientes = parseInt(formulario.ingredientes.value);
-    total += total + 5; 
+
+    if (formulario.queso.checked) { total += 5; }
+    if (formulario.lechuga.checked) { total += 5; }
+    if (formulario.jitomate.checked) { total += 5; }
+    if (formulario.cebolla.checked) { total += 5; }
+    if (formulario.jalapeño.checked) { total += 5; }
+    if (formulario.huevo.checked) { total += 5; }
+    if (formulario.pepino.checked) { total += 5; }
+    if (formulario.champiñones.checked) { total += 5; }
+    if (formulario.tocino.checked) { total += 5; }
+    if (formulario.dobleQueso.checked) { total += 5; }
+    if (formulario.piña.checked) { total += 5; }
+    if (formulario.guacamole.checked) { total += 5; }
+    if (formulario.bbq.checked) { total += 5; }
+    if (formulario.chipotle.checked) { total += 5; }
+    if (formulario.mayonesa.checked) { total += 5; }
+    if (formulario.mostaza.checked) { total += 5; }
+    if (formulario.ketchup.checked) { total += 5; }
+    if (formulario.pepperoni.checked) { total += 5; }
+    if (formulario.arugula.checked) { total += 5; }
+    if (formulario.cebollaCaramelizada.checked) { total += 5; }
 
     var cantidad = parseInt(formulario.cantidad.value);
-    total +=total * 2;
+    total = total * cantidad;
 
-    let total = (esp + pan + (ingredientes * 5)) * cantidad;
     if (esDomicilio) {
     total += 30;
     }
 
-    formulario.total.value = total;
+
+    document.getElementById('total').innerText = total;
 }
